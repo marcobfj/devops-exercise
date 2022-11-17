d1b0a3caa198b0486f62c4bc61409d96a65e4b3f
Marco Barron <marco.barron@cargamos.com>
2022-11-17 15:20:04 -0600

    Added "Extra Points"

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from multiprocessing import Value
 
+counter = Value('i', 0)
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!!"
-
+@app.route("/", methods = ['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        with counter.get_lock():
+            counter.value += 1
+            out = counter.value
+        return ('', 204)
+    else:
+        out = counter.value
+        return jsonify(count=out)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
